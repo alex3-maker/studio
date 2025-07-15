e06f2fcb5ebb1b180dae7878d8f91fbb8c3f9077
Alejandro Miranda <alejandro.mirnu@gmail.com>
2025-07-15 06:06:00 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/dashboard/duel-list.tsx
+++ src/components/dashboard/duel-list.tsx
@@ -59,7 +59,7 @@ export default function DuelList({ duels }: DuelListProps) {
             ))}
           </TableBody>
         </Table>
-      </Content>
+      </CardContent>
     </Card>
   );
 }
